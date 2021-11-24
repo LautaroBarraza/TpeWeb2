4c53116001acd3b07e9af043f754d7f24ad1e3fd
lautaro <89427372+LautaroBarraza@users.noreply.github.com>
2021-11-24 16:02:14 -0300

    Create e8d31dd7869f9bb4a8428e5a4e9a9d383ce45649_0.file.nav.tpl.php

---
+++ templates_c/e8d31dd7869f9bb4a8428e5a4e9a9d383ce45649_0.file.nav.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-24 19:57:34
+  from 'C:\xampp\htdocs\web2\TpeWeb2\templates\nav.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_619e8b1e6fa0b4_36638334',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e8d31dd7869f9bb4a8428e5a4e9a9d383ce45649' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\web2\\TpeWeb2\\templates\\nav.tpl',
+      1 => 1637605331,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_619e8b1e6fa0b4_36638334 (Smarty_Internal_Template $_smarty_tpl) {
+?><a href="home">home</a> 
+<a href="logOut">LogOut</a><?php }
+}
